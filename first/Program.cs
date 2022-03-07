752546dd1b904330101e7834e418dda33fb5296a
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:50:22 +0800

    第一次作業

--- first/Program.cs
+++ first/Program.cs
@@ -6,7 +6,12 @@ namespace first
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            int total = (int)meal_cost+tip_percent+tax_percent;
+            Console.WriteLine("用餐總費用:"+total);
         }
     }
 }
