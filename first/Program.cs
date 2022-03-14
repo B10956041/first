a9d596a6273908acaa12eb02dee9de984de6a4e3
user <user@DESKTOP-6JBEF5K>
2022-03-14 11:20:21 +0800

    測試

--- first/Program.cs
+++ first/Program.cs
@@ -6,12 +6,54 @@ namespace first
     {
         static void Main(string[] args)
         {
-            double meal_cost = 100;
-            int tip_percent = 15;
-            int tax_percent = 8;
+            //Console.Write("請輸入要印的N:");
+            //string a = Console.ReadLine();
+            //int N = int.Parse(a);
 
-            int total = (int)meal_cost+tip_percent+tax_percent;
-            Console.WriteLine("用餐總費用:"+total);
+            //for (int i = 1; i <= N; i++)
+            //{
+            //    for (int y = 1; y <= i; y++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine();
+
+            //for (int i = 1; i <= N; i++)
+            //{
+            //    Console.WriteLine(N + "*" + i + "=" + N * i);
+            //}
+
+            //Console.WriteLine();
+
+            //for (int i = 1; i <= 9; i++)
+            //{
+            //    for (int y = 1; y <= 9; y++)
+            //    {
+            //        Console.Write(i + "*" + y + "=" + i * y + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            int[] a = new int[5];
+
+            for(int i =0; i<a.Length;i++)
+            {
+                Console.Write("請輸入a[" + i + "]的數字:");
+                string b = Console.ReadLine();
+                a[i] = int.Parse(b);
+               
+            }
+
+            Console.WriteLine(a[0]);
+            Console.WriteLine(a[1]);
+            Console.WriteLine(a[2]);
+            Console.WriteLine(a[3]);
+            Console.WriteLine(a[4]);
+
+
         }
     }
 }
