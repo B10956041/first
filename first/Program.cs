2acdb87a7a580ee88f2267a0444f184f02f65fb0
user <user@DESKTOP-6JBEF5K>
2022-03-14 12:05:19 +0800

    220314作業

--- first/Program.cs
+++ first/Program.cs
@@ -37,21 +37,66 @@ namespace first
             //    Console.WriteLine();
             //}
 
-            int[] a = new int[5];
+            //int[] a = new int[5];
 
-            for(int i =0; i<a.Length;i++)
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write("請輸入a[" + i + "]的數字:");
+            //    string b = Console.ReadLine();
+            //    a[i] = int.Parse(b);
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+            //Array.Resize(ref a, 10);
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.Write("請輸入a[" + i + "]的數字:");
+            //    string b = Console.ReadLine();
+            //    a[i] = int.Parse(b);
+            //}
+            //for (int i = 0; i < a.Length; i++)
+            //{
+            //    Console.WriteLine(a[i]);
+            //}
+
+
+           
+
+            try
             {
-                Console.Write("請輸入a[" + i + "]的數字:");
-                string b = Console.ReadLine();
-                a[i] = int.Parse(b);
-               
+                int[] Num = { 1, 2, 3, 4, 5 };
+                String[] StudentName = { "Jack", "Marry", "Tom", "Grace", "Alice" };
+                int[] Computer = { 80, 65, 100, 98, 63 };
+                int[] Program = { 75, 67, 93, 25, 82 };
+                int[] Animation = { 60, 62, 91, 50, 87 };
+
+                Console.Write("請輸入學生座號:");
+                string num = Console.ReadLine();
+                int StudentNum = int.Parse(num);
+
+                for (int i = 1; i <= StudentNum; i++)
+                {
+                    if (i == StudentNum)
+                    {
+                        Console.WriteLine("姓名:" + StudentName[i - 1]);
+                        Console.WriteLine("電腦概論:" + Computer[i - 1]);
+                        Console.WriteLine("程式設計:" + Program[i - 1]);
+                        Console.WriteLine("動畫設計:" + Animation[i - 1]);
+                    }
+                }
             }
 
-            Console.WriteLine(a[0]);
-            Console.WriteLine(a[1]);
-            Console.WriteLine(a[2]);
-            Console.WriteLine(a[3]);
-            Console.WriteLine(a[4]);
+            catch
+            {
+                Console.WriteLine("沒有這個座號");
+            }
+
+
+
+
 
 
         }
